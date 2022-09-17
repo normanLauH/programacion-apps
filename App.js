09a2cf93114937520ae7f546a4cc743f8209ab6a
NormanLauH <nlau@ucol.mx>
2022-09-17 16:26:11 -0500

    Navigation

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+import ProductosView from './vistas/productos';
+import ProductosAdd from './vistas/productos_add';
+
+function MyStack(){
+    return(
+           <Stack.Navigator>
+           <Stack.Screen name='productos' component={ProductosView} options={{
+               title: "Listado de Productos",
+               headerTittleAlign: "center",
+           }}/>
+           <Stack.Screen name='productos_add' component={ProductosAdd}/>
+           </Stack.Navigator>
+    )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+          <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
